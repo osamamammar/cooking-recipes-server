ee713aa821e0949aa0173631709cc5653812eacc
osamamammar <osamaammar29@gmail.com>
2022-04-28 14:41:18 +0200

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const router = require("./routes/router");
 const RouteNotFound = require("./controllers/RouteNotFound");
 const connectDB = require("./database/db");
@@ -19,6 +20,7 @@ app.use(express.static("public"));
 app.use(fileUpload());
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
