c729c6af540bbe5868706a586593a319074d43bb
osamamammar <osamaammar29@gmail.com>
2022-04-30 07:06:16 +0200

    test error request entity too large

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use(express.static("public"));
 app.use(fileUpload());
 
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 // Routes
 app.use(router);
