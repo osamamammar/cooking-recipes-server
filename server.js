71bde0fbfdc6a74d5c20f76b7760d6011196efba
osamamammar <osamaammar29@gmail.com>
2022-04-30 09:14:33 +0200

    test

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type ,multipart/form-data"
+  );
   next();
 });
 
