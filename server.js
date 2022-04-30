31ff3733174be120dbec5d7af415f871d8c44a67
osamamammar <osamaammar29@gmail.com>
2022-04-30 08:46:50 +0200

    test

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(
 // CORS security
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
