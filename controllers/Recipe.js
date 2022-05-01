e870a9be883e84a791b006919ebd4a1a0eeeb041
osamamammar <osamaammar29@gmail.com>
2022-05-01 20:05:26 +0200

    remove __dirname in uploadpath

--- controllers/Recipe.js
+++ controllers/Recipe.js
@@ -111,8 +111,7 @@ const uploadImage = async (req, res) => {
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     sampleFile = await req.files.img;
     uploadPath =
-      __dirname +
-      "/../public/images/" +
+      "public/images/" +
       `${uniqueName}` +
       `.${sampleFile.mimetype.split("/")[1]}`;
 
